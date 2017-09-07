c1849082402331b0b753228d1bda0c960660405f
Garvit Pahal <g10pahal@gmail.com>
2017-09-07 12:11:30 +0530

    Add queue data structure

---
+++ ds/queue/queue.go
@@ -0,0 +1,55 @@
+package queue
+
+import "github.com/gpahal/go-algorithms/ds/list"
+
+// Instance represents a queue instance implemented as a doubly linked list.
+type Instance struct {
+	l *list.DoublyLinkedList
+}
+
+// New returns a new queue instance with the given items enqueued/added to it at the end.
+func New(items ...int) *Instance {
+	i := &Instance{l: &list.DoublyLinkedList{}}
+	i.Enqueue(items...)
+	return i
+}
+
+// Length returns the number of items in the queue.
+func (i *Instance) Len() int {
+	return i.l.Len()
+}
+
+// Empty checks whether the queue is empty.
+func (i *Instance) Empty() bool {
+	return i.l.Empty()
+}
+
+// Clear deletes all the items from the queue.
+func (i *Instance) Clear() {
+	i.l.Clear()
+}
+
+// Values returns a slice of the items of the queue.
+func (i *Instance) Values() []int {
+	return i.l.Values()
+}
+
+// Enqueue adds the items at the end of the queue.
+func (i *Instance) Enqueue(items ...int) {
+	i.l.PushBack(items...)
+}
+
+// Dequeue removes the item from the front of the queue and returns it.
+func (i *Instance) Dequeue() (int, bool) {
+	el := i.l.PopFront()
+	if el == nil {
+		return 0, false
+	}
+
+	return el.Value, true
+}
+
+// Copy creates a new copy of the queue.
+func (i *Instance) Copy() *Instance {
+	return New(i.Values()...)
+}
---
+++ ds/queue/queue_test.go
@@ -0,0 +1,128 @@
+package queue_test
+
+import (
+	"testing"
+
+	"github.com/gpahal/go-algorithms/ds/queue"
+)
+
+func TestNew(t *testing.T) {
+	newQueue := queue.New(4, 5, 6)
+	if newQueue.Len() != 3 {
+		t.Errorf("New 4, 5, 6: expected Len to be 3, got %d", newQueue.Len())
+	}
+
+	tmpArr := newQueue.Values()
+	if !slicesEqual(tmpArr, []int{4, 5, 6}) {
+		t.Errorf("New 4, 5, 6: expected Values to be [4 5 6], got %v", tmpArr)
+	}
+}
+
+func TestInstance_Len(t *testing.T) {
+	newQueue := queue.New()
+	if newQueue.Len() != 0 {
+		t.Errorf("Len: expected Len to be 0, got %d", newQueue.Len())
+	}
+
+	newQueue.Enqueue(4, 5, 6)
+	if newQueue.Len() != 3 {
+		t.Errorf("Len: expected Len to be 3, got %d", newQueue.Len())
+	}
+}
+
+func TestInstance_Empty(t *testing.T) {
+	newQueue := queue.New()
+	if !newQueue.Empty() {
+		t.Error("Empty: expected Empty to be true, got false")
+	}
+
+	newQueue.Enqueue(4, 5, 6)
+	if newQueue.Empty() {
+		t.Error("Empty: expected Empty to be false, got true")
+	}
+}
+
+func TestInstance_Clear(t *testing.T) {
+	newQueue := queue.New(4, 5, 6)
+	newQueue.Clear()
+	if newQueue.Len() != 0 {
+		t.Errorf("Clear: expected Len to be 0, got %d", newQueue.Len())
+	}
+}
+
+func TestInstance_Values(t *testing.T) {
+	newQueue := queue.New()
+	tmpArr := newQueue.Values()
+	if len(tmpArr) != 0 {
+		t.Errorf("Values: expected Values to be [], got %#v", tmpArr)
+	}
+
+	newQueue.Enqueue(4, 5, 6)
+	tmpArr = newQueue.Values()
+	if !slicesEqual(tmpArr, []int{4, 5, 6}) {
+		t.Errorf("Values: expected Values to be [4 5 6], got %v", tmpArr)
+	}
+}
+
+func TestInstance_Enqueue(t *testing.T) {
+	newQueue := queue.New(4, 5, 6)
+	newQueue.Enqueue(7)
+	tmpArr := newQueue.Values()
+	if !slicesEqual(tmpArr, []int{4, 5, 6, 7}) {
+		t.Errorf("Enqueue 7: expected Values to be [4 5 6 7], got %v", tmpArr)
+	}
+}
+
+func TestInstance_Dequeue(t *testing.T) {
+	newQueue := queue.New(4, 5, 6)
+	newQueue.Enqueue(7)
+	val, ok := newQueue.Dequeue()
+	if !ok || val != 4 {
+		t.Errorf("Dequeue: expected Dequeue to return (4, true), got (%d, %t)", val, ok)
+	}
+
+	tmpArr := newQueue.Values()
+	if !slicesEqual(tmpArr, []int{5, 6, 7}) {
+		t.Errorf("Dequeue: expected Values to be [5 6 7], got %v", tmpArr)
+	}
+
+	newQueue.Clear()
+	val, ok = newQueue.Dequeue()
+	if ok || val != 0 {
+		t.Errorf("Dequeue: expected Dequeue to return (0, false), got (%d, %t)", val, ok)
+	}
+}
+
+func TestInstance_Copy(t *testing.T) {
+	newQueue := queue.New(4, 5, 6)
+	copiedQueue := newQueue.Copy()
+	tmpArr := copiedQueue.Values()
+	if !slicesEqual(tmpArr, []int{4, 5, 6}) {
+		t.Errorf("Copy: expected Values to be [4 5 6], got %v", tmpArr)
+	}
+
+	copiedQueue.Enqueue(3)
+	tmpArr = copiedQueue.Values()
+	if !slicesEqual(tmpArr, []int{4, 5, 6, 3}) {
+		t.Errorf("Copy: expected Values to be [4 5 6 3], got %v", tmpArr)
+	}
+
+	tmpArr = newQueue.Values()
+	if !slicesEqual(tmpArr, []int{4, 5, 6}) {
+		t.Errorf("Copy: expected Values of original queue to be [4 5 6], got %v", tmpArr)
+	}
+}
+
+func slicesEqual(arr1 []int, arr2 []int) bool {
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	for i, el := range arr1 {
+		if el != arr2[i] {
+			return false
+		}
+	}
+
+	return true
+}
