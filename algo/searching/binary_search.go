f461eeea28184ab1ef8aa909b5f8a1f1fb4f0129
Garvit Pahal <g10pahal@gmail.com>
2017-10-01 20:27:19 +0530

    Add comments to searching algorithms

--- algo/searching/binary_search.go
+++ algo/searching/binary_search.go
@@ -1,11 +1,13 @@
 package searching
 
-// BinarySearch searches the sorted slice for the key using the binary search algorithm. If the key is not
-// found, -1 is returned.
+// BinarySearch searches the sorted slice arr for the key using the binary search algorithm. If the key is not found,
+// -1 is returned.
 func BinarySearch(arr []int, key int) int {
 	return binarySearchHelper(arr, key, 0)
 }
 
+// binarySearchHelper searches the sorted slice arr for the key using the binary search algorithm and adds offset
+// idxToBeAdded to the resulting index. If the key is not found, -1 is returned.
 func binarySearchHelper(arr []int, key int, idxToBeAdded int) int {
 	arrLength := len(arr)
 	if arrLength == 0 {
@@ -14,11 +16,15 @@ func binarySearchHelper(arr []int, key int, idxToBeAdded int) int {
 
 	mid := arrLength / 2
 	midVal := arr[mid]
+
 	if midVal == key {
+		// Found key.
 		return mid + idxToBeAdded
 	} else if midVal > key {
+		// Search the first half of the slice.
 		return binarySearchHelper(arr[:mid], key, idxToBeAdded)
 	} else {
+		// Search the second half of the slice and also increment value of the offset idxToBeAdded.
 		return binarySearchHelper(arr[mid+1:], key, idxToBeAdded+mid+1)
 	}
 }
