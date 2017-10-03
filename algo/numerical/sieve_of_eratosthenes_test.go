941fa81c6f89e5f36d5d1eb905b16b902abfbfb1
Garvit Pahal <g10pahal@gmail.com>
2017-10-03 22:37:49 +0530

    Add tests for sieve of eratosthenes algorithm

---
+++ algo/numerical/sieve_of_eratosthenes_test.go
@@ -0,0 +1,47 @@
+package numerical_test
+
+import (
+	"testing"
+
+	"github.com/gpahal/go-algorithms/algo/numerical"
+)
+
+func TestSieveOfEratosthenes(t *testing.T) {
+	cases := []struct {
+		limit int
+		sieve []bool
+	}{
+		{1, []bool{true, true}},
+		{4, []bool{true, true, false, false, true}},
+		{15, []bool{true, true, false, false, true, false, true, false, true, true, true, false, true, false, true, true}},
+		{0, nil},
+		{1 << 28, nil},
+	}
+
+	for _, c := range cases {
+		sieve := numerical.SieveOfEratosthenes(c.limit)
+		if !sliceEquals(sieve, c.sieve) {
+			t.Errorf("SieveOfEratosthenes %d: expected sieve to be %v, got %v", c.limit, c.sieve, sieve)
+		}
+	}
+}
+
+func sliceEquals(arr1 []bool, arr2 []bool) bool {
+	if arr1 == nil && arr2 == nil {
+		return true
+	}
+	if arr1 == nil || arr2 == nil {
+		return false
+	}
+	if len(arr1) != len(arr2) {
+		return false
+	}
+
+	for i, item := range arr1 {
+		if arr2[i] != item {
+			return false
+		}
+	}
+
+	return true
+}
