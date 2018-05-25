fba6a6996eafd4bdd19a700c7d2b9e01158847da
Garvit Pahal <g10pahal@gmail.com>
2018-05-25 21:25:20 +0530

    Simplify SliceStack implementation

--- ds/stack/slice_stack..go
+++ ds/stack/slice_stack..go
@@ -2,9 +2,7 @@ package stack
 
 // SliceStack represents a stack instance implemented as a slice used ascircular buffer.
 type SliceStack struct {
-	arr   []int
-	start int
-	size  int
+	arr []int
 }
 
 // NewSliceStack returns a new slice stack instance with the given items pushed into it.
@@ -20,29 +18,27 @@ func newSliceStack(items ...int) *SliceStack {
 
 // Len returns the number of items in the stack.
 func (s *SliceStack) Len() int {
-	return s.size
+	return len(s.arr)
 }
 
 // Empty checks whether the stack is empty.
 func (s *SliceStack) Empty() bool {
-	return s.size == 0
+	return len(s.arr) == 0
 }
 
 // Clear deletes all the items from the stack.
 func (s *SliceStack) Clear() {
 	s.arr = nil
-	s.start = 0
-	s.size = 0
 }
 
 // Top returns the top/last pushed element of the stack. If the stack is empty, second return
 // value is false.
 func (s *SliceStack) Top() (int, bool) {
-	if s.size == 0 {
+	if len(s.arr) == 0 {
 		return 0, false
 	}
 
-	return s.arr[(s.start+s.size-1)%len(s.arr)], true
+	return s.arr[len(s.arr)-1], true
 }
 
 // Push pushes the given items to the stack.
@@ -51,66 +47,29 @@ func (s *SliceStack) Push(items ...int) {
 		return
 	}
 
-	for i := 0; i < len(s.arr)-s.size && i < len(items); i++ {
-		s.arr[(s.start+s.size+i)%len(s.arr)] = items[i]
-	}
-	if len(items) <= len(s.arr)-s.size {
-		s.size += len(items)
-		return
-	}
-
-	// update items and s.size
-	items = items[len(s.arr)-s.size:]
-	s.size = len(s.arr)
-
-	// slice extension needed
-	if s.start == 0 {
-		s.arr = append(s.arr, items...)
-		s.size = len(s.arr)
-		return
-	}
-
 	s.arr = append(s.arr, items...)
-	j := s.size
-	for i := 0; i < s.start; i++ {
-		s.arr[j%len(s.arr)] = s.arr[i]
-		j++
-	}
-	for i := 0; i < len(items); i++ {
-		s.arr[j%len(s.arr)] = items[i]
-		j++
-	}
-	s.size = len(s.arr)
 }
 
 // Pop pops out an item from the stack in LIFO (Last In First Out) order. If the stack is empty,
 // second return value is false.
 func (s *SliceStack) Pop() (int, bool) {
-	if s.size == 0 {
+	if len(s.arr) == 0 {
 		return 0, false
 	}
 
-	v := s.arr[(s.start+s.size-1)%len(s.arr)]
-	s.size--
+	v := s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
 	return v, true
 }
 
 // Copy creates a new copy of the stack.
 func (s *SliceStack) Copy() Interface {
 	newStack := newSliceStack()
-	if s.size == 0 {
+	if len(s.arr) == 0 {
 		return newStack
 	}
 
-	newStack.start = 0
-	newStack.size = s.size
-	newStack.arr = make([]int, s.size)
-	if s.start+s.size > len(s.arr) {
-		copy(newStack.arr[:len(s.arr)-s.start], s.arr[s.start:len(s.arr)])
-		copy(newStack.arr[len(s.arr)-s.start:], s.arr[:s.start+s.size-len(s.arr)])
-	} else {
-		copy(newStack.arr, s.arr[s.start:])
-	}
-
+	newStack.arr = make([]int, len(s.arr))
+	copy(newStack.arr, s.arr)
 	return newStack
 }
